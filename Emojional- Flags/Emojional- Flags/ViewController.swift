dd9e089fecd9002ccf8f4a0b998451090a1b4a0a
Apple <apple@Apples-MacBook-Pro-170.local>
2019-07-25 16:44:50 -0700

    new project

--- Emojional- Flags/Emojional- Flags/ViewController.swift
+++ Emojional- Flags/Emojional- Flags/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Pagetitle: UILabel!
+    
+    @IBOutlet weak var Text1: UITextField!
+    
+    @IBOutlet weak var Text2: UITextField!
+    
+    @IBOutlet weak var Text3: UITextField!
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func Button1(_ sender: Any) {
+        if let newfile = Text1.text {
+            Pagetitle.text = newfile
+            
+        }
+    }
+    
+    @IBAction func Button2(_ sender: Any) {
+    }
+    
+    @IBAction func Button3(_ sender: Any) {
+    }
 }
 
